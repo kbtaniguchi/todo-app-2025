edba6fad554fb641bd5a80860cca0dbe25d52c84
Kiminobu Taniguchi <taniguchi.kiminobu@actier.co.jp>
2025-02-06 09:47:00 +0000

    addボタンを追加

--- app.vue
+++ app.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <button @click="add">
+      add
+    </button>
+    <input type="text" v-model="input" />
+    <ul>
+      <li v-for="item in list" :key="item">
+        {{ item }}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script lang="ts" setup>
+const input = ref<string>('')
+const list = ref<string[]>([])
+
+const add = () => {
+  list.value.push(input.value)
+}
+</script>
