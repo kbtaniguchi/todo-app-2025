2e41d04ee06da87867334a3216ea8ecb1ca5afcc
Kiminobu Taniguchi <taniguchi.kiminobu@actier.co.jp>
2025-02-06 10:03:46 +0000

    チェックボックスをつける + removeボタンをつける

--- app.vue
+++ app.vue
@@ -5,19 +5,32 @@
       add
     </button>
     <input type="text" v-model="input" />
-    <ul>
-      <li v-for="item in list" :key="item">
-        {{ item }}
+    <ol>
+      <li v-for="item in list" :key="item.index">
+        <input type="checkbox" v-model="item.isChecked" />
+        {{ item.title }}
+        <button @click="remove(item.index)">remove</button>
       </li>
-    </ul>
+    </ol>
   </div>
 </template>
 
 <script lang="ts" setup>
 const input = ref<string>('')
-const list = ref<string[]>([])
+
+type Task = {
+  isChecked: boolean,
+  title: string,
+  index: number
+}
+const list = ref<Task[]>([])
 
 const add = () => {
-  list.value.push(input.value)
+  list.value.push({ isChecked: false, title: input.value, index: list.value.length })
+  input.value = ''
+}
+
+const remove = (index: number) => {
+  list.value.splice(index, 1)
 }
 </script>
